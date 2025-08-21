cdfc2c4c755aa288cea42e308a5b64165efccfb3
sergisaimora <165085936+sergisaimora@users.noreply.github.com>
2025-08-21 21:27:10 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -617,7 +617,7 @@ const fetchWithTimeout = (url, options = {}, timeout = APP_CONFIG.FETCH_TIMEOUT_
 
 const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
     const byteCharacters = atob(b64Data);
-    const byteArrays = [];
+    const byteArrays: Uint8Array[] = [];
     for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
         const slice = byteCharacters.slice(offset, offset + sliceSize);
         const byteNumbers = new Array(slice.length);
